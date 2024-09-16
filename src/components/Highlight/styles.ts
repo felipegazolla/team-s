7d8cc8c7135f37eecde1bff325c792c1e1dcc183
felipe <enggazolla@gmail.com>
2024-09-16 20:37:12 -0300

    groupcard

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -8,6 +8,8 @@ export const Container = styled.View`
 export const Title = styled.Text`
   text-align: center;
 
+  margin-bottom: 6px;
+
   font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
   font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
   color: ${({ theme }) => theme.COLORS.WHITE};
